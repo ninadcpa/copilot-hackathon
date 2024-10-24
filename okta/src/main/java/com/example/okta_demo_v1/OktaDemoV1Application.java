7cd221a16576f0946d8cb4efa39262e169c71b7b
sandeepkp1175 <sandeep.pattnaik@gmail.com>
2024-10-24 16:53:36 -0500

    Update OktaDemoV1Application.java

--- okta/src/main/java/com/example/okta_demo_v1/OktaDemoV1Application.java
+++ okta/src/main/java/com/example/okta_demo_v1/OktaDemoV1Application.java
@@ -2,24 +2,21 @@ package com.example.okta_demo_v1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@Configuration
 public class OktaDemoV1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OktaDemoV1Application.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
