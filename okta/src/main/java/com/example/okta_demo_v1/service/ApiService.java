6d5c64d4bbf7bf6ccf586df74bf49612e2fb7911
sandeepkp1175 <sandeep.pattnaik@gmail.com>
2024-10-24 16:51:27 -0500

    Update ApiService.java

--- okta/src/main/java/com/example/okta_demo_v1/service/ApiService.java
+++ okta/src/main/java/com/example/okta_demo_v1/service/ApiService.java
@@ -1,12 +1,11 @@
 package com.example.okta_demo_v1.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Base64;
 
 @Service
 public class ApiService {
@@ -14,22 +13,32 @@ public class ApiService {
     @Autowired
     private OktaTokenService oktaTokenService;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     public String callApi() {
         String accessToken = oktaTokenService.getAccessToken();
 
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(accessToken);
+        headers.set("JWT-TOKEN", accessToken);
+
+        String[] chunks = accessToken.split("\\.");
+
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+
+        String header = new String(decoder.decode(chunks[0]));
+        String payload = new String(decoder.decode(chunks[1]));
+
 
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.exchange(
-                "https://api.example.com/endpoint",
-                HttpMethod.GET,
-                entity,
-                String.class
-        );
+        String reqPayload= "{\"userid\": \"user1\"}";
 
-        return response.getBody();
+        RequestEntity<String> requestEntity = new RequestEntity<>(reqPayload, headers, HttpMethod.POST, java.net.URI.create("https://8jip41mb78.execute-api.us-west-2.amazonaws.com/v1/token"));
+
+        ResponseEntity<String> responseEntity = restTemplate.exchange(requestEntity, String.class);
+
+        return responseEntity.getBody();
     }
 }
